2d762bda33b957b9ffab70b2c9fbf97a7c7aa8bc
Vaibhav <jayakumar.vaibhav@gmail.com>
2026-05-14 00:28:24 +0530

    changes

---
+++ Pandas/students.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    ID INTEGER,
+    Name TEXT,
+    Mark INTEGER
+);
+
+INSERT INTO students VALUES (1, 'Arun', 85);
+INSERT INTO students VALUES (2, 'Binu', 78);
+INSERT INTO students VALUES (3, 'Chitra', 92);
